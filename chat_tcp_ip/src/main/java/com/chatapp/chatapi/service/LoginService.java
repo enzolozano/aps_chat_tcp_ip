1b25a67fecf1add19c23448375d1293864115a5b
enzo_lozano <enzo.malozano@gmail.com>
2024-05-11 17:22:36 -0300

    Remove JWT and SQL packages

--- chat_tcp_ip/src/main/java/com/chatapp/chatapi/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.chatapp.chatapi.service;
-
-import com.chatapp.chatapi.model.LoginRequest;
-
-public interface LoginService {
-    
-    void autenticate(LoginRequest loginRequest);
-}
--- chat_tcp_ip/src/main/java/com/chatapp/chatapi/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.chatapp.chatapi.service;
-
-import com.chatapp.chatapi.entity.User;
-
-public interface UserService {
-    
-    User getUserByLogin(String username, String password);
-}
